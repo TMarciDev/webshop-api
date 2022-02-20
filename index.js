f25b17d020b01e0b34e92c347cb4444c89d0a8e0
Tóth Márton <ejjb7g@inf.elte.hu>
2022-02-20 21:47:57 +0100

    2_0:21:31

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console. log("DB connection succesful!"))
+  .catch((err) => console.log(err));
+
+
+app.use(express.json())
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Background server is running!");
+});
