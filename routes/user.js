971adbe41683d6f38fab41ddd678790fcbe3d39b
Tóth Márton <ejjb7g@inf.elte.hu>
2022-02-21 00:15:46 +0100

    2_1:08:32

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,30 @@
+const { json } = require("express/lib/response");
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-  res.send("user test is succesful");
-});
+//USER UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SEC
+    ).toString();
+  }
 
-router.post("/userposttest", (req,res) => {
-    const username = req.body.username
-    res.send("your username is: " + username)
-})
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
