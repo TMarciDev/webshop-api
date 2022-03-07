c349352d3322a367bb339c70f47a57e3a6ded04e
Tóth Márton <ejjb7g@inf.elte.hu>
2022-03-07 21:21:06 +0100

    stripe done

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+//const stripe = require("stripe")(process.env.STRIPE_KEY);
+const Stripe = require("stripe");
 
 router.post("/payment", (req, res) => {
-  stripe.charge.create(
+  const stripe = Stripe(process.env.STRIPE_KEY);
+  stripe.charges.create(
     {
       source: req.body.tokenId,
       amount: req.body.amount,
@@ -12,8 +14,9 @@ router.post("/payment", (req, res) => {
     (stripeErr, stripeRes) => {
       if (stripeErr) {
         res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
       }
-      res.status(200).json(stripeRes);
     }
   );
 });
